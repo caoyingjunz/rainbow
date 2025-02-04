3f62d3be01c9a2208ac60f4cd7f6241eda2761ee
caoyingjunz <cao.yingjunz@gmail.com>
2025-02-04 14:59:12 +0800

    add

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -1,1 +1,11 @@
 package template
+
+import "github.com/caoyingjunz/rainbow/cmd/app/options"
+
+type PluginTemplateConfig struct {
+	Default    options.DefaultOption    `yaml:"default"`
+	Kubernetes options.KubernetesOption `yaml:"kubernetes"`
+	Plugin     options.PluginOption     `yaml:"plugin"`
+	Register   options.Register         `yaml:"registry"`
+	Images     []string                 `yaml:"images"`
+}
