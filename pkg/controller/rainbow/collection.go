a58e24686c2537d18a994e973ffd7a10dcbf098f
caoyingjunz <cao.yingjunz@gmail.com>
2025-03-01 23:46:01 +0800

    add

--- pkg/controller/rainbow/collection.go
+++ pkg/controller/rainbow/collection.go
@@ -3,9 +3,39 @@ package rainbow
 import (
 	"context"
 
+	"k8s.io/klog/v2"
+
 	"github.com/caoyingjunz/rainbow/pkg/types"
 )
 
 func (s *ServerController) GetCollection(ctx context.Context, listOption types.ListOptions) (interface{}, error) {
-	return nil, nil
+	var (
+		taskNum   int64
+		imageNum  int64
+		reviewNum int64
+		err       error
+	)
+
+	taskNum, err = s.factory.Task().Count(ctx)
+	if err != nil {
+		klog.Error("获取任务数量失败: %v", err)
+	}
+	imageNum, err = s.factory.Image().Count(ctx)
+	if err != nil {
+		klog.Error("获取镜像数量失败: %v", err)
+	}
+
+	reviewes, err := s.factory.Task().ListReview(ctx)
+	if err != nil {
+		klog.Error("获取浏览数量失败: %v", err)
+	}
+	for _, review := range reviewes {
+		reviewNum = +review.Count
+	}
+
+	return map[string]int64{
+		"tasks":  taskNum,
+		"images": imageNum,
+		"review": reviewNum,
+	}, nil
 }
