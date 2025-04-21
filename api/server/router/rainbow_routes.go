c7772266514d4b6acff9b77d3a9132be46c10498
caoyingjunz <cao.yingjunz@gmail.com>
2025-04-21 22:32:45 +0800

    add

--- api/server/router/rainbow_routes.go
+++ api/server/router/rainbow_routes.go
@@ -1,8 +1,9 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"strings"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/caoyingjunz/pixiulib/httputils"
 	"github.com/caoyingjunz/rainbow/pkg/types"
