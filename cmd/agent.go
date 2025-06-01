06a2ef57aab59e1e676072840a2eb266c811eb2c
caoyingjunz <cao.yingjunz@gmail.com>
2025-06-02 00:28:52 +0800

    add

--- cmd/agent.go
+++ cmd/agent.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"flag"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"k8s.io/klog/v2"
 
+	pb "github.com/caoyingjunz/rainbow/api/rpc/proto"
 	"github.com/caoyingjunz/rainbow/cmd/app/options"
 )
 
@@ -31,4 +34,33 @@ func main() {
 			klog.Fatal("failed to rainbow agent: ", err)
 		}
 	}
+
+	conn, err := grpc.Dial(opts.ComponentConfig.Agent.RpcServer, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		klog.Fatalf("failed to connect rpc server %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewTunnelClient(conn)
+	stream, err := c.Connect(context.Background())
+	if err != nil {
+		klog.Fatalf("%v", err)
+	}
+	klog.Infof("agent connected to server by rpc")
+
+	clientId := opts.ComponentConfig.Agent.Name
+	// 启动协程，接受服务段回调 client 的请求
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				klog.Errorf("Receive error: %v", err)
+				return
+			}
+			// TODO
+			klog.Infof("node(%s) received from server: %s", clientId, msg.Result)
+		}
+	}()
+
+	select {}
 }
