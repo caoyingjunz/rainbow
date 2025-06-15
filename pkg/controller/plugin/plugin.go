8abb6895ebe656d2e70f59f3d7b001bb1917ff92
caoyingjunz <cao.yingjunz@gmail.com>
2025-06-15 18:48:32 +0800

    add

--- pkg/controller/plugin/plugin.go
+++ pkg/controller/plugin/plugin.go
@@ -240,11 +240,15 @@ func (p *PluginController) Complete() error {
 		p.Cfg.Plugin.Driver = DockerDriver
 	}
 
+	// 执行前校验
+	msgResult := "数据校验完成"
 	status, msg, process := "初始化成功", "初始化环境结束", 1
 	var err error
 	if err = p.doComplete(); err != nil {
+		msgResult = fmt.Sprintf("数据校验失败，原因：%v", err)
 		status, msg, process = "初始化失败", err.Error(), 3
 	}
+	p.CreateTaskMessage(msgResult)
 	p.SyncTaskStatus(status, msg, process)
 	return err
 }
@@ -379,14 +383,22 @@ func (p *PluginController) Run() error {
 
 	for _, runner := range p.Runners {
 		name := runner.GetName()
-		if err := runner.Run(); err != nil {
+		msgResult := name + "完成"
+
+		err := runner.Run()
+		if err != nil {
+			msgResult = fmt.Sprintf("%s失败，原因：%v", name, err)
+			p.CreateTaskMessage(msgResult)
 			p.SyncTaskStatus(name, name+"失败", 3)
 			return err
 		} else {
+			p.CreateTaskMessage(msgResult)
 			p.SyncTaskStatus(name, name+"完成", 1)
 		}
 	}
+
 	p.SyncTaskStatus("开始同步镜像", "", 1)
+	p.CreateTaskMessage("开始同步镜像")
 
 	klog.Infof("待推送镜像列表为 %v", p.Images)
 
@@ -404,9 +416,11 @@ func (p *PluginController) Run() error {
 
 			err := p.doPushImage(image)
 			if err != nil {
+				p.CreateTaskMessage(fmt.Sprintf("镜像 %s 同步失败，原因: %v", image.Name, err))
 				errCh <- err
 				return
 			}
+			p.CreateTaskMessage(fmt.Sprintf("镜像 %s 同步完成", image.Name))
 		}(imageToPush)
 	}
 	wg.Wait()
@@ -493,13 +507,17 @@ func (p *PluginController) CreateImages(names []string) ([]model.Image, error) {
 	return resp.Result, err
 }
 
-func (p *PluginController) CreateTaskMessage(msg string) error {
+func (p *PluginController) CreateTaskMessage(msg string) {
 	if !p.Synced {
-		return nil
+		return
 	}
 
-	return p.httpClient.Post(
+	if err := p.httpClient.Post(
 		fmt.Sprintf("%s/rainbow/%d/messages", p.Callback, p.TaskId),
 		nil,
-		map[string]interface{}{"message": msg})
+		map[string]interface{}{"message": msg}); err != nil {
+		klog.Errorf("创建 %s 失败 %v", msg, err)
+	} else {
+		klog.Infof("创建 %s 成功", msg)
+	}
 }
