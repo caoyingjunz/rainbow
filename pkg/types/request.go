4515c62824a3ed10a625e8b412b4d5d9febb9cd8
caoyingjunz <cao.yingjunz@gmail.com>
2025-09-06 22:36:28 +0800

    优化订阅查询机制

--- pkg/types/request.go
+++ pkg/types/request.go
@@ -290,10 +290,13 @@ type (
 
 		Path       string        `json:"path"`   // 默认会自动填充命名空间，比如 nginx 表示 library/nginx， jenkins/jenkins 则直接使用
 		Enable     bool          `json:"enable"` // 启动或者关闭
-		Size       int           `json:"size"`   // 同步最新多少个版本
+		Size       int           `json:"size"`   // 同步最新多少个版本, 最多 100 个，普通用户 5
 		RegisterId int64         `json:"register_id"`
 		Namespace  string        `json:"namespace"`
-		Interval   time.Duration `json:"interval"` // 间隔多久同步一次
+		Interval   time.Duration `json:"interval"`   // 间隔多久同步一次
+		ImageFrom  string        `json:"image_from"` // 镜像来源，支持 dockerhub, gcr, quay.io
+		Policy     string        `json:"policy"`     // 默认定义所有版本镜像，支持正则表达式，比如 v1.*
+		Arch       string        `json:"arch"`       // 支持的架构，默认不限制  linux/amd64
 	}
 
 	UpdateSubscribeRequest struct {
