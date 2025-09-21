7b3ed5a926fc45289a627a9141aa1831c6f28ecd
caoyingjunz <cao.yingjunz@gmail.com>
2025-09-22 00:06:49 +0800

    add

--- pkg/controller/rainbow/agent.go
+++ pkg/controller/rainbow/agent.go
@@ -314,6 +314,7 @@ func (s *AgentController) SearchQuayTags(ctx context.Context, req types.RemoteTa
 }
 
 func (s *AgentController) SearchDockerhubTags(ctx context.Context, req types.RemoteTagSearchRequest) ([]byte, error) {
+	// https://docs.docker.com/reference/api/registry/latest/#tag/Manifests
 	// repo=langgenius/dify-api
 	// token="$(curl -fsSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$repo:pull" | jq --raw-output '.token')"
 	// curl -s -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/$repo/tags/list"
